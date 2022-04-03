023bb4a196cd9420854eb992ad34abeb540d4113
sciberbee <yby0907200@gmail.com>
2022-04-04 00:21:57 +0900

    Finally Solved this problem(L24) and checked that my ListNode class workes fine. Thanks to https://leetcode.com/problems/swap-nodes-in-pairs/discuss/1775033/SWAPPING-NODES-(Not-just-the-values)-oror-Visual-Explanation-oror-Well-Explained-oror-C%2B%2B
    연결리스트 답답해 죽겠다 1. 디버깅이 힘듬 그런점에서 레플 디버거는 킹갓황 엄청직관적임
    2. 포인터질이 헷갈려 정신나갈것같음
    3. 답지 설명을 봐야되는데 벌써부터 어지러움

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
 from ListNode import ListNode
 param_1 = ListNode.convert_from_list(ListNode, [1, 2, 3, 4])
 
+
 class Solution:
     def swapPairs(self, head):
         if not head or not head.next:
             return head
         # Preprocessing
 
-        n1 = head
-        n2 = head.next
-        back = n1
+        DUMMY = ListNode(next=head)
+        prev = DUMMY
+        curr = prev.next
 
-        head = n2  # we won't use head until return
+        head = head.next
+        # Variable initialization
 
-        n1.next = n2.next
-        n2.next = n1
-        # First Time
-        while back.next.next:
-            back = n1
-            n1 = back.next
-            n2 = back.next.next
+        while curr and curr.next:  # 변수는 prev,curr,next만 사용하기 #아니 대체왜 curr.next 말고 curr도 체크해야 하는 거야??
             # ------------Wall--------------
-            back.next = n2  # curr points n2
-            n1.next = n2.next  # n2 points n1
-            n2.next = n1  # n1 points n2's next
+            prev.next = curr.next
+            curr.next = prev.next.next
+            prev.next.next = curr
             # ------------Bigger Wall---------
+            prev = curr
+            curr = prev.next
 
         return head
 
 swap = Solution.swapPairs(Solution, param_1)
-print(param_1)
-#
