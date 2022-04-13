820af372b285f995b799eee2499b098869f8829f
Seunghyeon Bang <yby0907200@gmail.com>
2022-04-13 23:04:04 +0900

    Solved P22, and learned stack methodlogy.

--- Chapter 09/P22L739.py
+++ Chapter 09/P22L739.py
@@ -1,17 +1,38 @@
 # from ~ import ~
 
-param_1 =
+param_1 = [73,74,75,71,69,72,76,73]
 # Started at Tue Apr 12 23:13:42 KST 2022
 # Suspended at Wed Apr 13 00:00:26 KST 2022 47 min elapsed.
-# Finished at
+# Finished at Wed Apr 13 01:00:10
 """
-My Idea :
+My Idea : for each index, search to the end till warm point(index) appears.
 """
 """
 What I Learned :
+I. Initializing. 
+We can initilize answer as [0] * length(T), so that we don't have to push 0 if answer doesn't appear.
+take this approach as... The *DEFAULT Outcome* is 0(impossible), and we change it to answer(date diff) only if we found answer.
+I think I'd better memorize this approach and code snippet //answer = [0] * len(T)//.
+
+II. Usage of stack.
+while stack and T[i] > T[stack[-1]]:
+    day = stack.pop()
+    answer[day] = i - day
+stack.append(i)
+
+var i is current index, and we check for processible days from stack.
+and for each day, we update the answer as many as possible at current index.
+checking stack emptiness prevents index error by shortcircuiting.
+
+I'd better memorize...
+while stack and T[i] > T[stack[-1]]:
+and
+(stack check)
+(push current val to stack)
+order.
 """
 
-
+""" #literally, delicious spagetti code
 class Solution:
     @classmethod
     def function(cls, temperatures):  # Construct delta_stack
@@ -48,6 +69,41 @@ class Solution:
         answer_stack.append(0)  # Process temperatures[-1]
 
         return answer_stack
+ """
+def old_sol(T):
+    answer = []
+    for i in range(len(T) - 1):
+        found = False
+        for j in range(i+1, len(T)):
+            if T[i] < T[j]:
+                answer[i] = j - i
+                break
+        if not found: answer.append(0)
+    answer.append(0)    
+    return answer
+
+def old_sol_withpointI(T):
+    answer = [0] = len(T)
+    for i in range(len(T) - 1):
+        for j in range(i+1, len(T)):
+            if T[i] < T[j]:
+                answer[i] = j - i
+                break
+    return answer
+
+def sol(T):
+    answer = [0] * len(T)
+    stack = []
+
+    for i in range(len(T)):
+        while stack and T[i] > T[stack[-1]]: 
+            #Processing all days(indices) able at this point(i).
+            #checking for stack prevents IndexOutOfRange by shortcircuiting.
+            pop = stack.pop()
+            answer[pop] = i - pop
+        stack.append(i)
+    return answer
+             
 
 
-print(Solution.function(param_1))
+print(sol(param_1))
