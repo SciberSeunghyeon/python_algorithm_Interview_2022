91078a53c507eb0a75f485b919d1b53664f49d9d
Seunghyeon Bang <yby0907200@gmail.com>
2022-04-14 00:16:25 +0900

    Update P23L225.py
    improved P23 answer.

--- Chapter 09/P23L225.py
+++ Chapter 09/P23L225.py
@@ -1,5 +1,7 @@
 #spent 30 min.
 
+import collections
+
 class MyStack:
     def __init__(self):
         self.q1 = collections.deque()
@@ -29,3 +31,21 @@ class MyStack:
 		
     def empty(self) -> bool:
         return not self.q1
+
+class MyStack_improved:
+    def __init__(self):
+        self.q = collections.deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+		
+    def empty(self) -> bool:
+        return not self.q
