9bc17ff9fe23be964dadf62cff26457fda787915
Seunghyeon Bang <yby0907200@gmail.com>
2022-04-16 23:29:54 +0900

    Update P25L622.py

--- Chapter 09/P25L622.py
+++ Chapter 09/P25L622.py
@@ -3,7 +3,7 @@
 # and of course, I have to stop and find another plan, if one approach seems to not be the way.
 
 
-"""
+""" Failed Linked List Solution.
 class Node:
     def __init__(self, val = None, next = None):
         self.val = val
@@ -51,6 +51,51 @@ class MyCircularQueue:
     def isFull(self) -> bool:
         return self.rear.next == self.front and self.rear.val
 """
+"""
+[1, 2, 3, None, None]
+front     rear
+Queue Empty/Full: two ptrs are same
+
+Important Code Snippet: index update.
+idx = (idx + 1)mod N
+0 <= mod N <= N-1.
+
+Elegant solution.
+"""
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.Q = [None] * k
+        self.k = k
+        self.front = self.rear = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull(): return False
+        
+        self.Q[self.rear] = value
+        self.rear = (self.rear + 1) % len(self.Q)
+        return True
+    
+    def deQueue(self) -> bool:
+        if self.isEmpty(): return False
+        
+        self.Q[self.front] = None
+        self.front = (self.front + 1) % len(self.Q)
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty(): return -1
+        return self.Q[self.front]
+
+    def Rear(self) -> int:
+        if self.isEmpty(): return -1
+        return self.Q[self.rear - 1]
+
+    def isEmpty(self) -> bool:
+        return self.front == self.rear and self.Q[self.front] == None
+
+    def isFull(self) -> bool:
+        return self.front == self.rear and self.Q[self.front] != None
 
 # Your MyCircularQueue object will be instantiated and called as such:
 # obj = MyCircularQueue(k)
